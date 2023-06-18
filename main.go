32e5c4f1da729d98580ff6f67cdf03e864fb213b
arunura <arun.iit@gmail.com>
2023-06-18 06:46:36 +0100

    Push access logs to bigquery

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	"context"
 	_ "embed"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fastly/compute-sdk-go/fsthttp"
+	"github.com/fastly/compute-sdk-go/geo"
+	"github.com/fastly/compute-sdk-go/rtlog"
 )
 
 //go:embed static/info.html
@@ -20,9 +26,31 @@ const (
 	BASIC_SCHEMA string = "Basic "
 )
 
+type LogRecord struct {
+	Timestamp            string   `json:"timestamp"`
+	ClientIP             string   `json:"client_ip"`
+	GeoData              *geo.Geo `json:"geo_data"`
+	Host                 string   `json:"host"`
+	Url                  string   `json:"url"`
+	Referer              string   `json:"request_referer"`
+	Method               string   `json:"request_method"`
+	Protocol             string   `json:"request_protocol"`
+	UserAgent            string   `json:"request_user_agent"`
+	FastlyPop            string   `json:"fastly_pop"`
+	FastlyServiceVersion string   `json:"fastly_service_version"`
+	FastlyTraceId        string   `json:"fastly_trace_id"`
+}
+
 func main() {
 
 	fsthttp.ServeFunc(func(ctx context.Context, w fsthttp.ResponseWriter, r *fsthttp.Request) {
+
+		fmt.Fprintf(os.Stdout, "Request received at: %v\n", time.Now().UTC())
+		err := log_request_to_bigquery(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error logging request to BigQuery: %v\n", err)
+		}
+
 		// Filter requests that have unexpected methods.
 		if r.Method == "POST" || r.Method == "PUT" || r.Method == "PATCH" || r.Method == "DELETE" {
 			w.WriteHeader(fsthttp.StatusMethodNotAllowed)
@@ -104,10 +132,72 @@ func main() {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			fmt.Fprintln(w, strings.ReplaceAll(info_html, "$fsv$", fsv))
 			return
+		} else if r.URL.Path == "/log" {
+			log_record, err := construct_log_record(r)
+			if err != nil {
+				w.WriteHeader(fsthttp.StatusInternalServerError)
+				fmt.Fprintf(w, "Error constructing log record: %v\n", err)
+				return
+			}
+
+			json_log_record, err := json.Marshal(log_record)
+			if err != nil {
+				w.WriteHeader(fsthttp.StatusInternalServerError)
+				fmt.Fprintf(w, "Error marshalling log record: %v\n", err)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			mw := io.MultiWriter(os.Stdout, w)
+			fmt.Fprintln(mw, string(json_log_record))
+			return
 		}
 
 		// Catch all other requests and return a 404.
 		w.WriteHeader(fsthttp.StatusNotFound)
 		fmt.Fprintf(w, "The page you requested could not be found\n")
 	})
+}
+
+func log_request_to_bigquery(r *fsthttp.Request) error {
+	log_record, err := construct_log_record(r)
+	if err != nil {
+		return err
+	}
+
+	json_log_record, err := json.Marshal(log_record)
+	if err != nil {
+		return err
+	}
+
+	endpoint := rtlog.Open("bigquery_website_usage_dwnld_zip")
+	mw := io.MultiWriter(os.Stdout, endpoint)
+	_, err = fmt.Fprintln(mw, string(json_log_record))
+	return err
+}
+
+func construct_log_record(r *fsthttp.Request) (*LogRecord, error) {
+
+	ip := net.ParseIP(r.RemoteAddr)
+	g, err := geo.Lookup(ip)
+	if err != nil {
+		return nil, err
+	}
+
+	log_record := &LogRecord{
+		Timestamp:            time.Now().UTC().Format("2006-01-02T15:04:05.000000Z"),
+		ClientIP:             r.RemoteAddr,
+		GeoData:              g,
+		Host:                 r.Host,
+		Url:                  r.URL.String(),
+		Referer:              r.Header.Get("Referer"),
+		Method:               r.Method,
+		Protocol:             r.Proto,
+		UserAgent:            r.Header.Get("User-Agent"),
+		FastlyPop:            os.Getenv("FASTLY_POP"),
+		FastlyServiceVersion: os.Getenv("FASTLY_SERVICE_VERSION"),
+		FastlyTraceId:        os.Getenv("FASTLY_TRACE_ID"),
+	}
+
+	return log_record, nil
 }
